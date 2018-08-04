9fd57fdf0c7c1f1a9163a30b3aa803bb60a9219b
zxzthlxf <zxzthlxf@hotmail.com>
2018-08-04 09:22:59 +0800

    add

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.template.loader import get_template
+#from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 from .models import Post
 
 # Create your views here.
 def homepage(request):
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists = list()
-    for count,post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    html = template.render(locals())
+#    post_lists = list()
+#    for count,post in enumerate(posts):
+#        post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>")
+#        post_lists.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
+    return HttpResponse(html)
