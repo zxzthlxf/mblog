345ee7e0fde81c79b81702c3783fe86fe5dd3e91
zxzthlxf <zxzthlxf@hotmail.com>
2018-08-06 03:01:38 +0800

    add

--- mainsite/views.py
+++ mainsite/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.template.loader import get_template
 #from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from datetime import datetime
 from .models import Post
@@ -18,3 +19,13 @@ def homepage(request):
 #        post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>")
 #        post_lists.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
     return HttpResponse(html)
+
+def showpost(request,slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
